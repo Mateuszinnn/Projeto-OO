8c9c9244e6a8998761f3eefd6d7a139bdbe88a7d
davi matias <69313657+daves22@users.noreply.github.com>
2021-10-13 14:10:24 -0300

    Atualização da classe Pessoas

--- Projeto-OO/src/app/Pessoas.java
+++ Projeto-OO/src/app/Pessoas.java
@@ -1,5 +1,63 @@
 package app;
 
-public class Pessoas {
+import javax.swing.JOptionPane;
 
+public class Pessoas {
+	private String nome;
+	private String email;
+	private float totRendimento;
+	private int numPessoas;
+	
+	public Pessoas(String nomePessoa, String emailPessoa, float totRend) {
+		this.nome = nomePessoa;
+		this.email = emailPessoa;
+		this.totRendimento = totRend;
+		this.numPessoas = 0;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public float getTotRendimento() {
+		return totRendimento;
+	}
+
+	public void setTotRendimento(float totRendimento) {
+		this.totRendimento = totRendimento;
+	}
+	public int getNumPessoas() {
+		return numPessoas;
+	}
+//metodos especiais
+	public void cadastrarPessoas() {
+		Pessoas[] pessoas = new Pessoas[0];
+		nome = JOptionPane.showInputDialog("Informe o nome da pessoa:");
+		email = JOptionPane.showInputDialog("Informe o email da pessoa:");
+		String strTotRend = JOptionPane.showInputDialog("Informe o rendimento total da pessoa ");
+		float totRendimento = Float.parseFloat(strTotRend);
+		this.numPessoas = this.numPessoas++;
+		Pessoas p = new Pessoas(nome,email,totRendimento);
+		
+		Pessoas[] tempP = new Pessoas[pessoas.length+1];
+		for (int i=0; i<pessoas.length; i++) {
+			tempP[i] = pessoas[i];
+		}
+		tempP[pessoas.length] = p;
+		
+		pessoas = tempP;
+		
+	}
 }
