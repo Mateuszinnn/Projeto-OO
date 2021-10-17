c231478e0f71b64070a7a14fc3f86b171af3cd5a
Mateuszinnn <mateusdealmeidadias@gmail.com>
2021-10-17 15:00:27 -0300

    opções no menu para calculo de despesas

--- Projeto-OO/src/app/Main_App.java
+++ Projeto-OO/src/app/Main_App.java
@@ -4,12 +4,14 @@ import javax.swing.JOptionPane;
 import Cadastros.Pessoas;
 import Cadastros.Despesas;
 import Cadastros.Categoria;
+import Calculos.Calculos;
 
 public class Main_App {
 
 	static Pessoas pessoa= new Pessoas();
 	static Despesas despesa = new Despesas();
 	static Categoria categoria= new Categoria();
+	static Calculos calculo= new Calculos();
 
 	public static void main(String[] args){
 		
@@ -20,6 +22,8 @@ public class Main_App {
 					+ "1 - Cadastrar Pessoas\n"
 					+ "2 - Cadastrar Despesas\n"
 					+ "3 - Cadastrar Categoria\n"
+					+ "4 - Calcular repartição de despesas - Regra igualitária\n"
+					+ "5 - Calcular repartição de despesas - Regra proporcional\n"
 					+ "0 - Sair do programa");
 			opcao = Integer.parseInt(strOpcao);
 
@@ -35,7 +39,14 @@ public class Main_App {
 			case 3: 
 				categoria.cadastrarCategoria();
 				break;
-
+				
+			case 4:
+				JOptionPane.showMessageDialog(null, calculo.calculoRegraIgualitaria());
+				break;
+				
+			case 5:
+				
+				break;
 			case 0: 
 				//sair do programa
 				break;
