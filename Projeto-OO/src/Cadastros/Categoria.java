022c1d28de3b4999e16d8b7fb618165630e9c8ee
Maciel Júnior <macielfcjunior@icloud.com>
2021-10-23 17:22:11 -0300

    Reformulação classe Categoria

--- Projeto-OO/src/Cadastros/Categoria.java
+++ Projeto-OO/src/Cadastros/Categoria.java
@@ -1,33 +1,9 @@
 package Cadastros;
 
+import java.util.List;
+
 import javax.swing.JOptionPane;
 
 public class Categoria {
-	protected String subCategoria;
-	
-	
-	
-	public Categoria() {
-		
-	}
 
-	public String getNomeSubCategoria() {
-		return subCategoria;
-	}
-
-//Metodos especiais
-	public void cadastrarCategoria() {
-		 Categoria[] categorias = new Categoria[0];
-		 subCategoria = JOptionPane.showInputDialog("Informe o nome da subcategoria de");
-		 
-		 Categoria c = new Categoria();
-		 
-		 Categoria[] tempC = new Categoria[categorias.length+1];
-			for (int i=0; i<categorias.length; i++) {
-				tempC[i] = categorias[i];
-			}
-			tempC[categorias.length] = c;
-			
-			categorias = tempC;
-	}
 }
