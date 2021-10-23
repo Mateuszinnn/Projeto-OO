99f64992ff911486051207ed6cc5064ba54d45e5
Maciel Júnior <macielfcjunior@icloud.com>
2021-10-23 17:24:52 -0300

    Adicionando construtor a classe Categoria

--- Projeto-OO/src/Cadastros/Categoria.java
+++ Projeto-OO/src/Cadastros/Categoria.java
@@ -1,9 +1,17 @@
 package Cadastros;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JOptionPane;
 
 public class Categoria {
+	protected String descricao;
+	protected List<String> subCategoria = new ArrayList<String>();
+
+	public Categoria(String descricao, List<String> subCategoria) {
+		this.descricao = descricao;
+		this.subCategoria = subCategoria;
+	}
 
 }
