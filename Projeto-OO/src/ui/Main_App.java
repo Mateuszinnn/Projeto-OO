113020e31be302cc4935b0905099197cd7b65136
beatrizSO <bia.santosdoliveira@gmail.com>
2021-10-25 18:42:16 -0300

    Main sem caracteres especiais

--- Projeto-OO/src/ui/Main_App.java
+++ Projeto-OO/src/ui/Main_App.java
@@ -20,13 +20,13 @@ public class Main_App {
 		pessoa.lerAlunos();
 		despesa.lerDespesas();
 		do { 
-			String strOpcao = JOptionPane.showInputDialog("Bem vindo ao App da República! \n\n"
-					+ "Escolha uma opção: \n\n"
+			String strOpcao = JOptionPane.showInputDialog("Bem vindo ao App da Republica! \n\n"
+					+ "Escolha uma opcao: \n\n"
 					+ "1 - Cadastrar Pessoas\n"
 					+ "2 - Cadastrar Despesas\n"
 					+ "3 - Cadastrar Categoria\n"
-					+ "4 - Calcular repartição de despesas - Regra igualitária\n"
-					+ "5 - Calcular repartição de despesas - Regra proporcional\n"
+					+ "4 - Calcular reparticao de despesas - Regra igualitaria\n"
+					+ "5 - Calcular reparticao de despesas - Regra proporcional\n"
 					+ "0 - Sair do programa");
 			opcao = Integer.parseInt(strOpcao);
 
@@ -59,7 +59,7 @@ public class Main_App {
 
 			default:
 				//Opcao invalida
-				JOptionPane.showMessageDialog(null, "Opcão Inválida!");
+				JOptionPane.showMessageDialog(null, "Opcao Invalida!");
 				break;
 			}
 		} while (opcao != 0);
