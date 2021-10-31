4123072f2882dfd48473e13eef049c40687fffe1
Arthurlima544 <arthurlindaolima544@gmail.com>
2021-10-30 23:33:40 -0300

    alterando metodo ler arquivo

--- Projeto-OO/src/Cadastros/Functions.java
+++ Projeto-OO/src/Cadastros/Functions.java
@@ -1,8 +1,10 @@
 package Cadastros;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-
+import java.io.InputStreamReader;
 
 import javax.swing.JOptionPane;
 
@@ -23,4 +25,26 @@ public class Functions {
             JOptionPane.showMessageDialog(null, "Ocorreu uma falha durante a gravação do arquivo.");
         }
     }
+    public static String lerArquivo(String nomeDoArquivo) {
+        try {
+            BufferedReader myBuffer = new BufferedReader(
+                    new InputStreamReader(new FileInputStream(nomeDoArquivo), "UTF-8"));
+
+            String linha = myBuffer.readLine();
+
+            String categorias = "";
+            while (linha != null) {
+                categorias = categorias + linha + "\n";
+                linha = myBuffer.readLine();
+            }
+
+            myBuffer.close();
+            return categorias;
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Ocorreu uma falha durante a leitura do arquivo.");
+            return "";
+        }
+
+    }
+    
 }
