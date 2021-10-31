06939a0c260f819c5b7b8470ad2f0ed69e497f51
Arthurlima544 <arthurlindaolima544@gmail.com>
2021-10-30 23:57:31 -0300

    inserindo cadastrar categorias

--- Projeto-OO/src/Cadastros/Categoria.java
+++ Projeto-OO/src/Cadastros/Categoria.java
@@ -3,20 +3,59 @@ package Cadastros;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 public class Categoria {
 	protected String descricao;
-	protected List<String> subCategoria = new ArrayList<String>();
+	private List<String> subCategorias = new ArrayList<String>();
 
 	public Categoria(String descricao, List<String> subCategoria) {
 		this.descricao = descricao;
-		this.subCategoria = subCategoria;
+		this.subCategorias = subCategoria;
 	}
 
 	public String getNomeSubCategoria(int index) {
-		return subCategoria.get(index);
+		return subCategorias.get(index);
 	}
 	public void gravarCategoria() {
         Functions.gravarArquivo("categorias.txt", toString());
     }
+
+	public void cadastrarCategoria() {
+        String subCategoria, option;
+        Boolean isValid = false;
+
+        this.descricao = JOptionPane.showInputDialog("Informe a descrição da categoria:");
+
+        do {
+
+            if (this.subCategorias.size() == 0) {
+                option = JOptionPane.showInputDialog("Gostaria de Cadastrar uma subcategoria?\n1) - Sim\n2) - Não");
+            } else {
+                option = JOptionPane.showInputDialog("Gostaria de Cadastrar outra subcategoria?\n1) - Sim\n2) - Não");
+            }
+
+            isValid = Functions.isInteger(option, "Opção inválida! Tente novamente");
+
+            if (isValid) {
+                if (Functions.rangeOfValues(1, 2, Integer.parseInt(option))) {
+                    if (Integer.parseInt(option) == 1) {
+                        subCategoria = JOptionPane.showInputDialog("Digite a descrição da subcategoria: ");
+                        this.subCategorias.add(subCategoria);
+                        isValid = false;
+                    }
+                } else {
+                    isValid = false;
+                }
+            }
+
+        } while (!isValid);
+
+        if (isValid) {
+            JOptionPane.showMessageDialog(null, "Categoria cadastrada com sucesso");
+            gravarCategoria();
+        }
+    }
+
 
 }
